9d16f883058b99e01dfaf56ab5329bc3abf0509f
raj12963rahul <rahul.r20@iiits.in>
2023-08-16 19:10:19 +0530

    Question 1 isvalid URl implemented

--- Question_1/server.js
+++ Question_1/server.js
@@ -2,15 +2,32 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+
+function isValidURL(str) {
+    const pattern = new RegExp(
+        '^([a-zA-Z]+:\\/\\/)?' + 
+        '((([a-z\\d]([a-z\\d-][a-z\\d]))\\.)+[a-z]{2,}|' + 
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + 
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+])' + 
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + 
+        '(\\#[-a-z\\d_]*)?$', 
+        'i'
+    );
+    return pattern.test(str);
+}
+
 app.get('/numbers', async (req, res) => {
     try {
         const answer = new Set();
         const urlArray = req.query.url;
         for (let i = 0; i < urlArray.length; i++) {
-            const res = await axios.get(urlArray[i])
-            const numArray = res.data.numbers;
-            for (let j = 0; j < numArray.length; j++) {
-                answer.add(numArray[j]);
+            console.log(urlArray[i])
+            if (isValidURL(urlArray[i])) {
+                const res = await axios.get(urlArray[i])
+                const numArray = res.data.numbers;
+                for (let j = 0; j < numArray.length; j++) {
+                    answer.add(numArray[j]);
+                }
             }
         }
         const arr = [...answer].sort((a, b) => a - b);
@@ -22,8 +39,6 @@ app.get('/numbers', async (req, res) => {
         res.status(500).send('internal server error')
     }
 })
-
-
 
 app.listen(8008, () => {
     console.log("server started")
