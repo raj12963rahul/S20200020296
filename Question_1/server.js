979a96f5cc4c5f08a7c67acda9aeb5f173f4a677
raj06cool <raj06072000@gmail.com>
2023-08-16 19:49:13 +0530

    Modified the code for 500ms

--- Question_1/server.js
+++ Question_1/server.js
@@ -2,28 +2,51 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-
 function isValidURL(str) {
     const pattern = new RegExp(
-        '^([a-zA-Z]+:\\/\\/)?' + 
-        '((([a-z\\d]([a-z\\d-][a-z\\d]))\\.)+[a-z]{2,}|' + 
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + 
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+])' + 
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + 
-        '(\\#[-a-z\\d_]*)?$', 
+        '^([a-zA-Z]+:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-][a-z\\d]))\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+])' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', // fragment locator
         'i'
     );
     return pattern.test(str);
 }
+
+async function fetchUrl(url) {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw new Error(`Failed to fetch ${url}: ${error.message}`);
+    }
+}
+
+async function fetchAllUrls(urls) {
+    const promises = urls.map(fetchUrl);
+
+    try {
+        const result = await Promise.race([
+            Promise.all(promises),
+            new Promise((_, reject) => setTimeout(() => reject(new Error('Timed out')), 500))
+        ]);
+        return result;
+    } catch (error) {
+        throw new Error(`Request timed out: ${error.message}`);
+    }
+}
+
 
 app.get('/numbers', async (req, res) => {
     try {
         const answer = new Set();
         const urlArray = req.query.url;
         for (let i = 0; i < urlArray.length; i++) {
-            console.log(urlArray[i])
             if (isValidURL(urlArray[i])) {
                 const res = await axios.get(urlArray[i])
+
                 const numArray = res.data.numbers;
                 for (let j = 0; j < numArray.length; j++) {
                     answer.add(numArray[j]);
