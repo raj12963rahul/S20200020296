b9476b0106cec99b489d200f7ccf86490f13abf2
raj12963rahul <rahul.r20@iiits.in>
2023-08-16 18:59:53 +0530

    Question 1 Completed

--- Question_1/server.js
+++ Question_1/server.js
@@ -2,6 +2,27 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+app.get('/numbers', async (req, res) => {
+    try {
+        const answer = new Set();
+        const urlArray = req.query.url;
+        for (let i = 0; i < urlArray.length; i++) {
+            const res = await axios.get(urlArray[i])
+            const numArray = res.data.numbers;
+            for (let j = 0; j < numArray.length; j++) {
+                answer.add(numArray[j]);
+            }
+        }
+        const arr = [...answer].sort((a, b) => a - b);
+        res.status(200).send({
+            "numbers": arr
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('internal server error')
+    }
+})
+
 
 
 app.listen(8008, () => {
